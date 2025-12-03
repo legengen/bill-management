e6e9d27ea49b6774ea9ae699ebbdcdd68aff9e20
legengen <1991171956@qq.com>
2025-12-03 13:48:32 +0800

    feat(models): 添加用户、账单及相关模型

---
+++ src/common/models.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <chrono>
+
+// 储存了实体模型，用于保存数据库对应的映射
+namespace model {
+
+    using Timestamp = std::chrono::system_clock::time_point;
+
+    struct User {
+        std::string phone;
+        std::string username;
+        std::string password;
+        std::string role;
+        double balance = 0.0;
+        Timestamp createdAt;
+    };
+
+    struct Bill {
+        int id = 0;
+        std::string title;
+        std::string description;
+        double amount = 0.0;
+        Timestamp issueDate;
+        BillStatus status;
+        Event event;
+        Annotation annotation;
+    };
+    
+    // 事项模型，用于保存账单对应的事项
+    struct Event {
+        int id;
+        std::string name;
+    };
+
+    // 管理员注解
+    struct Annotation {
+        int id;
+        std::string content;
+        Timestamp createdAt;
+        int authorid;
+    };
+
+    enum BillStatus {
+        avaliable, frozen
+    };
+}
