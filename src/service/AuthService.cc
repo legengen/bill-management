a69a181d1693e3473a6a302bd01ac342ab183f96
legengen <1991171956@qq.com>
2025-12-03 15:29:36 +0800

    feat(service): 添加AuthService类，实现用户登录、注册和重置密码功能

---
+++ src/service/AuthService.cc
@@ -0,0 +1,29 @@
+#include "AuthService.h"
+
+std::optional<model::User> AuthService::Login(const std::string& phone, const std::string& password){
+    std::optional<model::User> user = user_repository->queryByPhone(phone);
+    if (!user.has_value() || user.value().password != password) {
+        return std::nullopt;
+    }
+    return user;
+}
+
+std::optional<model::User> AuthService::Register(const std::string& phone, const std::string& username, const std::string& password){
+    std::optional<model::User> user = user_repository->queryByPhone(phone);
+    if (user.has_value()) {
+        return std::nullopt;
+    }
+    model::User user(phone, username, password);
+    return user;
+}
+
+
+
+bool AuthService::ResetPassword(int userId, const std::string& oldPwd, const std::string& newPwd){
+    std::optional<model::User> user = user_repository->findById(userId);
+    if (user.value().password != oldPwd) {
+        return false;
+    }
+    user.value().password = newPwd;
+    return true;
+}
---
+++ src/service/AuthService.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../data/irepositories.h"
+
+class AuthService {
+public:
+    std::optional<model::User> Login(const std::string& phone, const std::string& password);
+    std::optional<model::User> Register(const std::string& phone, const std::string& username, const std::string& password);
+    bool ResetPassword(int userId, const std::string& oldPwd, const std::string& newPwd);
+
+private:
+    std::shared_ptr<repo::IUserRepository> user_repository;
+};
