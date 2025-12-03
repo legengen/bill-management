c79fc3640201e932e9762277f1b52d194099e6c0
legengen <1991171956@qq.com>
2025-12-03 20:24:59 +0800

    refactor(repository): 更新仓库接口以使用模型命名空间中的数据类型

--- src/data/irepositories.h
+++ src/data/irepositories.h
@@ -8,50 +8,49 @@
 
 // 声明了仓库接口，待数据层实现
 namespace repo {
-    using namespace model;
 
     struct IUserRepository {
         virtual ~IUserRepository() = default;
-        virtual void save(const User& u) = 0;
+        virtual void save(const model::User& u) = 0;
 
-        virtual std::optional<User> findById(int id) = 0; 
+        virtual std::optional<model::User> findById(int id) = 0; 
 
-        virtual std::optional<User> queryByPhone(const std::string& phone) = 0; // 仅管理员可用
+        virtual std::optional<model::User> queryByPhone(const std::string& phone) = 0; // 仅管理员可用
 
         virtual bool setBalanceByPhone(const std::string& phone) = 0; // 仅管理员可用
     };
 
     struct IBillRepository {
         virtual ~IBillRepository() = default;
-        virtual void save(const Bill& b) = 0;
+        virtual void save(const model::Bill& b) = 0;
 
-        virtual std::optional<Bill> findById(int id) = 0;
+        virtual std::optional<model::Bill> findById(int id) = 0;
 
-        virtual std::vector<Bill> queryByEvent(int ownerId, int eventId) = 0;
-        virtual std::vector<Bill> queryByEvent(std::string& name) = 0; // 仅管理员可用
+        virtual std::vector<model::Bill> queryByEvent(int ownerId, int eventId) = 0;
+        virtual std::vector<model::Bill> queryByEvent(std::string& name) = 0; // 仅管理员可用
 
-        virtual std::vector<Bill> queryByTime(int ownerId, Timestamp from, Timestamp to) = 0;
-        virtual std::vector<Bill> queryByTime(Timestamp from, Timestamp to) = 0; // 仅管理员可用
+        virtual std::vector<model::Bill> queryByTime(int ownerId, model::Timestamp from, model::Timestamp to) = 0;
+        virtual std::vector<model::Bill> queryByTime(model::Timestamp from, model::Timestamp to) = 0; // 仅管理员可用
 
-        virtual std::vector<Bill> queryByPhone(const std::string& phone) = 0; // 仅管理员可用
+        virtual std::vector<model::Bill> queryByPhone(const std::string& phone) = 0; // 仅管理员可用
         virtual void remove(int id) = 0;
     };
 
     struct IEventRepository {
         virtual ~IEventRepository() = default;
-        virtual void save(const Event& e) = 0; // 仅管理员可用
+        virtual void save(const model::Event& e) = 0; // 仅管理员可用
 
-        virtual std::optional<Event> findById(int Id) = 0; // 仅管理员可用
-        virtual std::optional<Event> findByName(int name) = 0; // 仅管理员可用
+        virtual std::optional<model::Event> findById(int Id) = 0; // 仅管理员可用
+        virtual std::optional<model::Event> findByName(int name) = 0; // 仅管理员可用
 
         virtual bool setStatusById(int Id) = 0; // 仅管理员可用
     };
 
     struct IAnnotationRepository {
         virtual ~IAnnotationRepository() = default;
-        virtual void save(const Annotation& a) = 0; // 仅管理员可用
+        virtual void save(const model::Annotation& a) = 0; // 仅管理员可用
 
-        virtual std::optional<Annotation> findById(int Id) = 0; 
+        virtual std::optional<model::Annotation> findById(int Id) = 0; 
     };
 
 }
