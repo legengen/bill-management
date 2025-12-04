aa59db45ecf9a2ece174f226f176fec0a7cab50d
legengen <1991171956@qq.com>
2025-12-04 13:46:05 +0800

    fix(event_service): 修改EventService中的event_repo_为event_repository_，并添加QueryById方法

--- src/services/EventService.cc
+++ src/services/EventService.cc
@@ -4,7 +4,20 @@ std::optional<model::Event> EventService::QueryByName(const std::string& name) {
     if (name.empty() || name == "") {
         return std::nullopt;
     }
-    auto e = event_repo_->findByName(name);
+    auto e = event_repository_->findByName(name);
+    if (!e.has_value()) {
+        return std::nullopt;
+    }
+
+    return e;
+}
+
+std::optional<model::Event> EventService::QueryById(int event_id) {
+    if (event_id <= 0) {
+        return std::nullopt;
+    }
+
+    auto e = event_repository_->findById(event_id);
     if (!e.has_value()) {
         return std::nullopt;
     }
@@ -17,20 +30,27 @@ std::optional<model::Event> EventService::CreateEvent(model::Event& e) {
         return std::nullopt;
     }
 
-    auto it = event_repo_->findByName(e.name);
+    auto it = event_repository_->findByName(e.name);
     if (it.has_value()) {
         return std::nullopt;
     }
 
-    event_repo_->save(e);
+    event_repository_->save(e);
+    return e;
 }
 
 bool EventService::SetStatus(int event_id, model::EventStatus status){
-    auto e = event_repo_->findById(event_id);
+    if (event_id <= 0) {
+        return false;
+    }
+
+    auto e = event_repository_->findById(event_id);
     if (!e.has_value()) {
         return false;
     }
 
-    event_repo_->setStatusById(event_id, status);
+    if (!event_repository_->setStatusById(event_id, status)) {
+        return false;
+    }
     return true;
 }
--- src/services/EventService.h
+++ src/services/EventService.h
@@ -2,10 +2,14 @@
 
 class EventService{
 public:
+    explicit EventService(std::shared_ptr<repo::IEventRepository> event_repo_):
+        event_repository_(event_repo_) {}
+
     std::optional<model::Event> QueryByName(const std::string& name);
+    std::optional<model::Event> QueryById(int event_id);
     std::optional<model::Event> CreateEvent(model::Event& e);
     bool SetStatus(int event_id, model::EventStatus status);
 private:
-    std::shared_ptr<repo::IEventRepository> event_repo_;
+    std::shared_ptr<repo::IEventRepository> event_repository_;
 };
 
