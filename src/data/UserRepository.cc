bcd4a071784a5c0e41522cd16727b4baa6a1df87
legengen <1991171956@qq.com>
2025-12-05 14:54:38 +0800

    feat(data): 添加了用户仓库的实现

---
+++ src/data/UserRepository.cc
@@ -0,0 +1,44 @@
+#include "UserRepositoryImpl.h"
+
+using namespace orm;
+
+void UserRepositoryImpl::save(const model::User& u) {
+    if (u.id == 0) {
+        db_->GetStorage().insert(u);
+    }
+
+    db_->GetStorage().update(u);
+}
+
+std::optional<model::User> UserRepositoryImpl::findById(int id) {
+    if (id <= 0) {
+        return std::nullopt;
+    } 
+
+    return db_->GetStorage().get_optional<model::User>(id);
+}
+
+std::optional<model::User> UserRepositoryImpl::queryByPhone(const std::string& phone) {
+    if (phone.empty()) {
+        return std::nullopt;
+    }
+
+    return db_->GetStorage().get_optional<model::User>(where(c(&model::User::phone) = phone));
+}
+
+std::vector<model::User> UserRepositoryImpl::queryByPhonePartial(const std::string& partial) {
+    if (partial.empty()) {
+        return {};
+    }
+
+    return db_->GetStorage().get_all<model::User>(where(like(&model::User::phone, partial)));
+}
+
+bool UserRepositoryImpl::setBalanceByPhone(const std::string& phone) {
+    if (phone.empty()) {
+        return false;
+    }
+
+    auto user = db_->GetStorage().get<model::User>(where(c(&model::User::phone) = phone));
+    db_->GetStorage().update(user);
+}
---
+++ src/data/UserRepositoryImpl.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "irepositories.h"
+#include "DatabaseORM.h"
+#include <memory>
+
+class UserRepositoryImpl : public repo::IUserRepository {
+public:
+    explicit UserRepositoryImpl(std::shared_ptr<DatabaseORM> db) : db_(db) {}
+    
+    void save(const model::User& u) override;
+    std::optional<model::User> findById(int id) override;
+    std::optional<model::User> queryByPhone(const std::string& phone) override;
+    std::vector<model::User> queryByPhonePartial(const std::string& partial) override;
+    bool setBalanceByPhone(const std::string& phone) override;
+    
+private:
+    std::shared_ptr<DatabaseORM> db_;
+};
